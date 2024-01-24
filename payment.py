387330810a44bcdcd58e2d7ecdc3db05b11874ed
xomyak-coder <dkhol@inbox.ru>
2024-01-24 22:42:51 +0300

    Fix bug in string

--- payment.py
+++ payment.py
@@ -8,7 +8,7 @@ def calculate (a, b):
 
 @app.route("/")
 def index():
-    return "This is a payment page! Plaese pasy %s USD"%(calculate(10, 20))"
+    return "This is a payment page! Please pay %s USD"%(calculate(10, 20))
 
 if __name__ =  "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
