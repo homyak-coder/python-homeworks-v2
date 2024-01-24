23c778b71d886a2b5ce910336fe932097aa5cb93
xomyak-coder <dkhol@inbox.ru>
2024-01-24 22:45:49 +0300

    fix =

--- payment.py
+++ payment.py
@@ -10,5 +10,7 @@ def calculate (a, b):
 def index():
     return "This is a payment page! Please pay %s USD"%(calculate(10, 20))
 
-if __name__ =  "__main__":
+if __name__ ==  "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
+
+    
